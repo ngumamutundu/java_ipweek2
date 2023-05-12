b1376a5df997e450f7699e592ea3448a075bee99
Pius <mutundu.nguma@gmail.com>
2023-05-12 09:35:45 +0300

    Correction done on README file

---
+++ src/main/java/org/javaipweek2/app/model/Hero.java
@@ -0,0 +1,49 @@
+package org.javaipweek2.app.model;
+
+public class Hero {
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+
+    public Hero(String name, int age, String specialPower, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+    }
+
+    // Getter and setter methods
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public void setSpecialPower(String specialPower) {
+        this.specialPower = specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+}
---
+++ src/main/java/org/javaipweek2/app/model/Squad.java
@@ -0,0 +1,38 @@
+package org.javaipweek2.app.model;
+public class Squad {
+    private int maxSize;
+    private String name;
+    private String cause;
+
+    public Squad(int maxSize, String name, String cause) {
+        this.maxSize = maxSize;
+        this.name = name;
+        this.cause = cause;
+    }
+
+    // Getter and setter methods
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+}
