4dc1810f076bf612ff78b615d5b364f04783d71d
Pius <mutundu.nguma@gmail.com>
2023-06-08 10:39:41 +0300

    Changing the layout display name

--- src/main/java/org/javaipweek2/app/db/database.java
+++ src/main/java/org/javaipweek2/app/db/database.java
@@ -5,7 +5,7 @@ import org.sql2o.Sql2o;
 public class database {
     private static final Sql2o connection = new Sql2o (
 
-            "jdbc:postgresql://localhost:5432/hero_squad",
+            "jdbc:postgresql://localhost:5432/herosquad",
             "postgres",
             "Maseki1992"
     );
